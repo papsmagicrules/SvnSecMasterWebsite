19789bc0f96a9882a8f1227e861250e97e468802
papsmagicrules <papsmagicrules@gmail.com>
2013-08-30 19:51:33 +0530

    Build v1.0
    Basic Functionality

---
+++ SvnWebsite_Test/Default.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <%--<meta runat="server" id="meta1" http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />--%>
+    <link rel="Stylesheet" href="Stylesheets/Defaultpage.css" />
+    <title></title>
+       
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="tdTop">
+        <asp:Image ID="secMasterLogo" ImageUrl="~/Images/IVP.gif" runat="server" />
+    </div>
+    
+    <div id="content" runat="server">
+        <center>
+            <asp:Panel ID="pnlBase" runat="server">
+                <table id="tblContent">
+                    <tr>
+                        <td style="width:200px;">
+                            <asp:Label ID="svnUrl" runat="server" Text="SVN Url" />
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtSvnUrl" Width="250px" runat="server" Text="http://ivp-build.ivp.co.in:8080/svn/SCM/SCM/BUILD/SECMASTER" />
+                            </td>
+                            <td style="color:Red; text-align:left;">*
+                            <asp:RequiredFieldValidator ID="rfvSvnUrl" runat="server" ControlToValidate="txtSvnUrl" ErrorMessage="Svn Url is Required" ValidationGroup="SvnCheck" />
+                            <asp:CustomValidator ID="customSvnUrl" runat="server" ControlToValidate="txtSvnUrl" 
+                                    ErrorMessage="Invalid Svn Url"
+                                    onservervalidate="customSvnUrl_ServerValidate" ValidationGroup="SvnCheck"/>
+                            </td>
+                        
+                    </tr>
+                    <tr>
+                        <td style="width:200px;">
+                            Choose
+                        </td>
+                        <td style="text-align: left;">
+                            <asp:RadioButtonList ID="optionList" runat="server" al>
+                                <asp:ListItem Text="Batch" Value="Batch" />
+                                <asp:ListItem Text="Updates" Value="Updates" Selected="true" />
+                                <asp:ListItem Text="Installation Build" Value="Install" />
+                            </asp:RadioButtonList>
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr><td></td><td></td>
+                    <td style="text-align:right;">
+                            <asp:Button ID="btnCheckSvn" runat="server" Text="Check Url" 
+                                onclick="btnCheckSvn_Click" ValidationGroup="SvnCheck"/>
+                        </td>
+                    </tr>
+                    </table>
+                    </center>
+                    <asp:Panel ID="updatePanel" runat="server" Visible="false">
+                    <center>
+                <table id="tblUpdatePanel">
+                <tr style="text-align: left;">
+                        <td id="test1" runat="server">
+                            <asp:Label ID="lblRevision" runat="server" Text="Revision ID" />
+                        </td>
+                        <td id="test2" runat="server">
+                            <asp:TextBox ID="txtRevision" Width="196px" runat="server" />
+                        </td>
+                        <td style="color:Red;" id="test3" runat="server">*
+                            <asp:RequiredFieldValidator ID="rfvRevision" runat="server" ControlToValidate="txtRevision" ErrorMessage="Revision is Required" ValidationGroup="RevisionCheck"/>
+                            </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: left;width:200px;">
+                             <asp:Label ID="lblSecMasterVersion" runat="server" Text="Security Master Version" />
+                        </td>
+                        <td style="text-align: left;">
+                            <asp:DropDownList ID="ddlSecMasterVersion" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlSecMasterVersion_SelectedIndexChanged" Width="200px" />
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: left;width:200px;">
+                             <asp:Label ID="lblRadVersion" runat="server" Text="RAD Version" />
+                        </td>
+                        <td style="text-align: left;">
+                            <asp:DropDownList ID="ddlRadVersion" Width="200px" runat="server" />
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: left;width:200px;">
+                             <asp:Label ID="lblRefMasterVersion" runat="server" Text="Ref Master Version" />
+                        </td>
+                        <td style="text-align: left;">
+                            <asp:DropDownList ID="ddlRefMasterVersion" Width="200px" runat="server" />
+                        </td>
+                        <td></td>
+                    </tr>
+                    <tr><td></td><td></td><td></td></tr>
+                    <tr><td></td><td></td><td></td></tr>
+                    
+                    <tr>
+                        <td>
+                        </td>
+                        <td></td>
+                        <td style="text-align: right;">
+                            <asp:Button ID="btnSubmit" Text="Submit" runat="server" OnClick="btnSubmit_Click" ValidationGroup="RevisionCheck"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: left;width:200px;">
+                            <asp:Label ID="lblDownload" runat="server" />
+                        </td>
+                        <td></td>
+                        <td style="text-align: right;">
+                            <asp:Button ID="Button1" Text="Download" runat="server" />
+                        </td>
+                    </tr>
+                </table>
+                </center>
+            </asp:Panel>
+        </asp:Panel>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ SvnWebsite_Test/Default.aspx.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.ObjectModel;
+using System.Text;
+using SharpSvn;
+using System.IO;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    string radioButtonSelectedItem = null;
+    Uri reposUri = null;
+    SvnClient client = null;
+    string secMasterVersion = null;
+    List<string> radVersion = null;
+    List<string> refMasterVersion = null;
+
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        client = new SvnClient();
+    }
+
+    protected Uri GetSvnUri()
+    {
+        Uri url = new Uri(txtSvnUrl.Text);
+        return url;
+    }
+
+    protected Uri GetAbsoluteUri(Uri repoPath)
+    {
+        string url = txtSvnUrl.Text;
+        string reppath = repoPath.ToString();
+        string result = url.Replace(reppath, "");
+        result = result + reppath;
+        return new Uri(result);
+    }
+
+    protected Uri GetAbsoluteUri(string relativePath)
+    {
+        string url = txtSvnUrl.Text;
+        string temp = url.Replace(relativePath, "");
+        StringBuilder result = new StringBuilder(temp + "/" +relativePath);
+        return new Uri(result.ToString());
+    }
+
+    protected void ReadConfigFile(string secmVersion)
+    {
+        string tempurl = GetSvnUri().ToString() + "/" + secmVersion + "/DependentVersions.xml";
+        Uri absolute = new Uri(tempurl);
+        Debug.WriteLine(absolute.ToString());
+
+        string temppath = @"C:\Users\athakur\Documents\Visual Studio 2010\WebSites\SvnWebsite_Test\Temp\";
+        string xmlLoadPath = temppath + @"\DependentVersions.xml";
+        SvnExportArgs exportargs = new SvnExportArgs { Overwrite = true };
+        client.Export(absolute, temppath, exportargs);
+
+        XElement xe = XElement.Load(xmlLoadPath);
+        if (!xe.IsEmpty)
+        {
+            IEnumerable<XElement> rad = xe.Elements("RAD");
+            foreach (XElement ra in rad)
+            {
+                radVersion = new List<string>();
+                XAttribute tempatt = ra.Attribute("version");
+                string[] str = tempatt.Value.Split(',');
+                Debug.Write("RAD Versions :");
+                foreach (string s in str)
+                {
+                    radVersion.Add(s);
+                }
+            }
+
+
+            IEnumerable<XElement> refm = xe.Elements("RefMaster");
+            foreach (XElement re in refm)
+            {
+                refMasterVersion = new List<string>();
+                XAttribute tempatt = re.Attribute("version");
+                string[] str = tempatt.Value.Split(',');
+                Debug.Write("RefMaster Versions :");
+                foreach (string s in str)
+                {
+                    refMasterVersion.Add(s);
+                }
+            }
+        }
+
+        ddlRefMasterVersion.DataSource = refMasterVersion;
+        ddlRefMasterVersion.DataBind();
+        ddlRadVersion.DataSource = radVersion;
+        ddlRadVersion.DataBind();
+    }
+
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        reposUri = GetAbsoluteUri(ddlSecMasterVersion.SelectedValue);
+        radioButtonSelectedItem = optionList.SelectedValue;
+
+        if (radioButtonSelectedItem == "Updates")
+        {
+            Collection<SvnLogEventArgs> logcollection = null;
+            SvnLogArgs logargs = null;
+
+            logargs = new SvnLogArgs { Range = new SvnRevisionRange(Int32.Parse(txtRevision.Text), SvnRevisionType.Head) };
+
+
+            client.GetLog(reposUri, logargs, out logcollection);
+
+            int i = 1;
+            foreach (SvnLogEventArgs logEventArgs in logcollection)
+            {
+                Debug.WriteLine("Entry " + i);
+
+
+                foreach (var changedpaths in logEventArgs.ChangedPaths)
+                {
+                    Uri absolute = GetAbsoluteUri(reposUri.ToString() + changedpaths.RepositoryPath.ToString());
+                    if (changedpaths.Action.ToString() != "Delete")
+                    {
+
+                        SvnExportArgs exportargs = new SvnExportArgs();
+                        exportargs.Overwrite = true;
+                        System.Diagnostics.Debug.WriteLine(changedpaths.Action);
+
+                        string[] directorytemp = changedpaths.RepositoryPath.ToString().Split('/');
+                        string directorypath = null;
+
+                        for (int j = 0; j < directorytemp.Length - 1; j++)
+                        {
+                            directorypath = directorypath + directorytemp[j] + '\\';
+                        }
+
+                        string temptarget = "C:\\Back Up\\Crap\\svnexport\\Updates" + directorypath;
+                        Directory.CreateDirectory(temptarget);
+
+                        string target = temptarget + directorytemp[directorytemp.Length - 1];
+                        Collection<SvnInfoEventArgs> info = null;
+                        bool flag = client.GetInfo(absolute, new SvnInfoArgs { ThrowOnError = false }, out info);
+                        if (flag)
+                        {
+                            client.Export(absolute, target, exportargs);
+                            Debug.WriteLine("File Created");
+                        }
+                    }
+                    Debug.WriteLine(changedpaths.RepositoryPath);
+                }
+
+                i++;
+                Debug.WriteLine("\n\n");
+            }
+
+        }
+
+
+        else if (radioButtonSelectedItem == "Install")
+        {
+            string storepath = "C:\\Back Up\\Crap\\svnexport\\Install";
+            SvnUpdateResult exportResult = null;
+            client.Export(reposUri, storepath, new SvnExportArgs { Overwrite = true, Revision = SvnRevision.Head },out exportResult);
+            Debug.WriteLine(exportResult.Revision);
+        }
+
+        else if (radioButtonSelectedItem == "Batch")
+        {
+            string storepath = "C:\\Back Up\\Crap\\svnexport\\Batch";
+            SvnUpdateResult exportResult = null;
+            client.Export(reposUri, storepath, new SvnExportArgs { Overwrite = true, Revision = new SvnRevision(long.Parse(txtRevision.Text))}, out exportResult);
+            Debug.WriteLine(exportResult.Revision);
+        }
+    }
+
+
+    protected void ddlSecMasterVersion_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        secMasterVersion = ddlSecMasterVersion.SelectedValue;
+        ReadConfigFile(secMasterVersion);
+
+
+    }
+
+
+    protected void btnCheckSvn_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            List<string> secmvers = new List<string>();
+            Uri absolute = GetSvnUri();
+            Collection<SvnInfoEventArgs> info = null;
+            
+            bool flag = client.GetInfo(absolute, new SvnInfoArgs { ThrowOnError = false }, out info);
+            if (flag)
+            {
+                updatePanel.Visible = true;
+                content.Style.Add("height", "370px");
+                //content.Style.Add("width", "700px");
+            }
+
+            else
+            {
+                updatePanel.Visible = false;
+            }
+
+            Collection<SvnListEventArgs> infoList = null;
+            client.GetList(absolute, out infoList);
+
+            foreach (SvnListEventArgs sev in infoList)
+            {
+                if (sev.Path != "")
+                {
+                    Uri temppath = GetAbsoluteUri(sev.Path + "/DependentVersions.xml");
+                    Collection<SvnInfoEventArgs> directorycollectioninfo = null;
+                    bool chkflag = client.GetInfo(temppath, new SvnInfoArgs { ThrowOnError = false }, out directorycollectioninfo);
+                    if (chkflag)
+                    {
+
+                        secmvers.Add(sev.Path);
+                    }
+                }
+            }
+
+            ddlSecMasterVersion.DataSource = secmvers;
+            ddlSecMasterVersion.DataBind();
+
+            secMasterVersion = secmvers[0];
+            ReadConfigFile(secMasterVersion);
+
+            radioButtonSelectedItem = optionList.SelectedValue;
+
+            if (radioButtonSelectedItem == "Install")
+            {
+                rfvRevision.Enabled = false;
+                test1.Style.Add("display", "none");
+                test2.Style.Add("display", "none");
+                test3.Style.Add("display", "none");
+            }
+
+            else
+            {
+                rfvRevision.Enabled = true;
+                test1.Style.Add("display", "table-cell");
+                test1.Style.Add("width", "120px");
+                test2.Style.Add("display", "table-cell");
+                test2.Style.Add("width", "200px");
+                test3.Style.Add("display", "table-cell");
+                test3.Style.Add("width", "200px");
+            }
+        }
+    }
+
+
+
+    protected void customSvnUrl_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        try
+        {
+            Uri tempp = new Uri(txtSvnUrl.Text);
+            Debug.WriteLine(tempp);
+            args.IsValid = true;
+        }
+        catch
+        {
+            Debug.WriteLine("Invalid Url");
+            args.IsValid = false;
+        }
+
+    }
+}
